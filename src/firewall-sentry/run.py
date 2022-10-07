90be26141bb25e2586e33d04fcfdec4543622ba0
Josh Freiermuth <josh.freiermuth@genmills.com>
2022-10-07 09:07:05 -0500

    redo quotes to double, get remote address with X-Forwarded-For

--- src/firewall-sentry/run.py
+++ src/firewall-sentry/run.py
@@ -3,44 +3,43 @@ from FirewallSentry import FirewallSentry
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-sentry = FirewallSentry(os.environ['TOKEN'])
+sentry = FirewallSentry(os.environ["TOKEN"])
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def firewall_manager():
     """
     Function to handle /
     :return: rendered /
     """
-    blank = ''
-    print(request.headers)
-    if request.method == 'GET':
+    blank = ""
+    if request.method == "GET":
         print(F"Sentry Token: {sentry.get_token()}; Provided headers{request.headers}")
-        if 'FS_TOKEN' in request.headers and request.headers['FS_TOKEN'] == sentry.get_token():
-            print('FS_TOKEN provided by {} is correct, returning list'.format(request.remote_addr))
+        if "FS-TOKEN" in request.headers and request.headers["FS-TOKEN"] == sentry.get_token():
+            print("FS-TOKEN provided by {} is correct, returning list".format(request.remote_addr))
             return sentry.list()
 
-    elif request.method == 'POST':
+    elif request.method == "POST":
         print(request.headers)
-        if 'FS_TOKEN' in request.headers and request.headers['FS_TOKEN'] == sentry.get_token():
-            if 'FS_HOST' in request.headers:
-                sentry.update(request.headers['FS_HOST'], request.remote_addr)
-                print('FS_TOKEN provided by {} is correct, host {} updated to {}'.format(
-                    request.remote_addr,
-                    request.headers['FS_HOST'],
-                    request.remote_addr
+        if "FS-TOKEN" in request.headers and request.headers["FS-TOKEN"] == sentry.get_token():
+            if 'FS-HOST' in request.headers:
+                sentry.update(request.headers["FS-HOST"], request.headers["X-Forwarded-For"])
+                print("FS-TOKEN provided by {} is correct, host {} updated to {}".format(
+                    request.headers["X-Forwarded-For"],
+                    request.headers["FS-HOST"],
+                    request.headers["X-Forwarded-For"]
                 ))
 
             else:
-                print('FS_TOKEN provided by {} is correct, but no FS_HOST provided'.format(request.remote_addr))
+                print("FS-TOKEN provided by {} is correct, but no FS-HOST provided".format(request.remote_addr))
 
         else:
-            print('Header provided by {} is incorrect'.format(request.remote_addr))
+            print("Header provided by {} is incorrect".format(request.remote_addr))
 
     return blank
 
-if __name__ == '__main__':
-    print('Development server detected')
-    print('Token: ' + sentry.get_token())
+if __name__ == "__main__":
+    print("Development server detected")
+    print("Token: " + sentry.get_token())
 
-    app.run(debug=True, host='0.0.0.0', port=8080, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=8080, use_reloader=False)
